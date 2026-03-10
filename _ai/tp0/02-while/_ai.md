23f0d5f5747264f924e2afb9f7dbc6741ad337ad
ccckmit <ccckmit@gmail.com>
2026-03-10 15:40:53 +0800

    add _ai/tp0c

--- _ai/tp0/02-while/_ai.md
+++
@@ -1,2 +0,0 @@
-
-https://aistudio.google.com/prompts/1u_w9IBEk7YnPuzvGum7EDbz-3bpybGE9
--- _ai/tp0/02-while/run.sh
+++
@@ -1,2 +0,0 @@
-RUST_BACKTRACE=1 ./target/release/tp0c $1.tp0
-clang -Wno-override-module $1.ll sys/runtime.c -o $1
